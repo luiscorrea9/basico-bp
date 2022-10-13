75a5c44e21102dfb2a4038ce693574c480661e1e
Luis Correa <Luiscorrea874@gmail.com>
2022-10-13 11:40:16 -0500

    fix warning

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Variables from './components/Variables';
 import Contador from './components/Contador';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
